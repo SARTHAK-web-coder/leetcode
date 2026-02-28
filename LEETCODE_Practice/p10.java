ff0d673eee8dd9c10cd89c1e3e239745a73b0775
Sarthak-web-coder <jsarthak014@gmail.com>
2026-02-28 12:09:54 +0530

    dublcate 2(size)

---
+++ LEETCODE_Practice/p10.java
@@ -0,0 +1,39 @@
+// Given an integer array nums and an integer k,
+// return true if there are two distinct indices i and j in the array
+// such that nums[i] == nums[j] and abs(i - j) <= k.
+// Example 1:
+
+// Input: nums = [1,2,3,1], k = 3
+// Output: true
+// Example 2:
+
+// Input: nums = [1,0,1,1], k = 1
+// Output: true
+// Example 3:
+
+// Input: nums = [1,2,3,1,2,3], k = 2
+// Output: false
+
+import java.util.HashSet;
+
+public class p10 {
+    public static void main(String[] args) {
+        int nums[] = { 1, 2, 3, 1, 2, 3 };
+        int k = 2;
+        System.out.println(containsNearbyDuplicate(nums, k));
+    }
+
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+}
