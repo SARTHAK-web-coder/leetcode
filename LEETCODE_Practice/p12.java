83df7e5ed4b9d17dc445f57f295c0be5655d2b69
Jagtap Sarthak <jsarthak014@gmail.com>
2026-03-01 20:45:07 +0530

    3rd max element
    Remove check for array length less than 3 in thirdMax method.

--- LEETCODE_Practice/p12.java
+++ LEETCODE_Practice/p12.java
@@ -23,9 +23,6 @@ public class p12 {
     public static int thirdMax(int[] nums) {
         Arrays.sort(nums);
         int n = nums.length;
-        if (n < 3) {
-            return nums[n - 1];
-        }
         int last = nums[n - 1];
         int distinctCount = 1;
         for (int i = n - 1; i >= 0; i--) {
