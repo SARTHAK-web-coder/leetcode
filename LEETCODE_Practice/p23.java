6c78eff1461d289544fe9b1d3ef8b16f68e67d2b
Sarthak-web-coder <jsarthak014@gmail.com>
2026-04-04 18:55:46 +0530

    valid anagram

---
+++ LEETCODE_Practice/p23.java
@@ -0,0 +1,28 @@
+
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+import java.util.Arrays;
+
+public class p23 {
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        System.out.println(isAnagram(s, t));
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if (n != m) {
+            return false;
+        }
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        for (int i = 0; i < n; i++) {
+            if (a[i] != b[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
