6b25a481c0622d673f7afc6c899a89e16cbc9842
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-08-12 12:10:30 +0530

    Time: 4 ms (96.89%) | Memory: 48 MB (36.53%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -6,7 +6,7 @@ class Solution {
         while (left < right) {
             int mid = left + (right - left) / 2;
             if(x - arr[mid] > arr[mid+k] - x){
-                left =  mid +1;
+                left =  mid + 1;
             }else{
                 right = mid;
             }
