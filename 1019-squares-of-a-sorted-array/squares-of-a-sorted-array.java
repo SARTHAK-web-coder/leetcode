bd233ee2e92e292e48ec030d9d45ea8a0747baac
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-02 22:31:41 +0530

    Time: 1 ms (100.00%) | Memory: 47.6 MB (35.46%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,21 +1,21 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         int n = nums.length;
-        for(int i=0;i<n;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        for(int i=0;i<n-1;i++){
-            int current = i;
-            for(int j=i+1;j<n;j++){
-                if(nums[current]>nums[j]){
-                    current=j;
-                }
+        int arr[] = new int[n];
+        int left = 0;
+        int right = n - 1;
+        for (int i = n - 1; i >= 0; i--) {
+            int leftSqu = nums[left] * nums[left];
+            int rightSqu = nums[right] * nums[right];
+            // comparing squares of left & right element , because they or non-decreasing order , so automatically get high square no.
+            if (leftSqu > rightSqu) {
+                arr[i] = leftSqu;
+                left++;
+            } else {
+                arr[i] = rightSqu;
+                right--;
             }
-            // swap 
-            int temp = nums[i];
-            nums[i]=nums[current];
-            nums[current]=temp;
         }
-        return nums;
+        return arr;
     }
 }
