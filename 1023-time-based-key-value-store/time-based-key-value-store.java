0dc62ba00325ddc23df65de7482637c3dde47da5
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-07 23:13:29 +0530

    Time: 144 ms (41.92%) | Memory: 108.5 MB (45.13%) - LeetSync

--- 1023-time-based-key-value-store/time-based-key-value-store.java
+++ 1023-time-based-key-value-store/time-based-key-value-store.java
@@ -2,48 +2,51 @@ class TimeValue {
     int timestamp;
     String value;
 
-    TimeValue(int timestamp, String value) {
+    TimeValue(int timestamp, String value){
         this.timestamp = timestamp;
         this.value = value;
     }
 }
 
 class TimeMap {
-    private HashMap<String, ArrayList<TimeValue>> map ;
+    private HashMap<String, ArrayList<TimeValue>> map;
+
     public TimeMap() {
         map = new HashMap<>();
     }
 
     public void set(String key, String value, int timestamp) {
         if (map.containsKey(key)) {
-            map.get(key).add(new TimeValue(timestamp, value));
+            map.get(key).add(new TimeValue(timestamp , value));
+
         } else {
             ArrayList<TimeValue> arr = new ArrayList<>();
             arr.add(new TimeValue(timestamp, value));
-            map.put(key, arr);
+            map.put(key ,arr);
         }
+
     }
 
     public String get(String key, int timestamp) {
         ArrayList<TimeValue> list = map.get(key);
-        if (list == null) {
+        if(list==null){
             return "";
         }
-        int left = 0;
-        int right = list.size() - 1;
+        int left =0;
+        int right = list.size()-1;
         String ans = "";
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (list.get(mid).timestamp == timestamp) {
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(list.get(mid).timestamp==timestamp){
                 ans = list.get(mid).value;
                 break;
-            } else if (list.get(mid).timestamp < timestamp) {
+            }else if(list.get(mid).timestamp<timestamp){
                 ans = list.get(mid).value;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
+                left = mid +1;
+            }else{
+                right = mid -1;
             }
-        }
+        } 
         return ans;
     }
 }
