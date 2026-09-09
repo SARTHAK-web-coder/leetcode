6ec26554eec704a799bdefc36724be975393d192
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-09 15:35:10 +0530

    Time: 4 ms (99.70%) | Memory: 44.2 MB (48.16%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        int[] s1freq = new int[26];
         int k = s1.length();
-        for (int i = 0; i < k; i++) {
-            char ch = s1.charAt(i);
-            s1freq[ch - 'a']++;
-        }
         int n = s2.length();
-        int i = 0;
-        while (i <= n - k) {
-            int l = i;
-            int r = i + k;
-            int[] s2freq = new int[26];
-            while (l < r) {
-                char ch = s2.charAt(l);
-                s2freq[ch - 'a']++;
-                l++;
-            }
-            // // Compare frequency String
-            if (Arrays.equals(s1freq, s2freq)) {
+        if (k > n) {
+            return false;
+        }
+        int a[] = new int[26];
+        int b[] = new int[26];
+        for (int i = 0; i < k; i++) {
+            a[s1.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < k; i++) {
+            b[s2.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(a, b)) {
+            return true;
+        }
+        for (int i = k; i < n; i++) {
+            b[s2.charAt(i) - 'a']++;
+            b[s2.charAt(i - k) - 'a']--;
+            if (Arrays.equals(a, b)) {
                 return true;
             }
-            i++;
         }
         return false;
     }
