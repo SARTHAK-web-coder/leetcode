23aa71918b385d698e357e409dc3b4fc62c5a096
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-09 11:34:16 +0530

    Time: 64 ms (19.41%) | Memory: 48.3 MB (5.40%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,30 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        int[] pFreq = new int[26];
-        int k = p.length();
-        for (int i = 0; i < k; i++) {
-            char ch = p.charAt(i);
-            pFreq[ch - 'a']++;
-        }
-        int n = s.length();
-        int i = 0;
-        while (i <= n - k) {
-            int[] windowFreq = new int[26];
-            int l = i;
-            int r = i + k;
-            int count = 0;
-            while (l < r) {
-                char ch = s.charAt(l);
-                windowFreq[ch - 'a']++;
-                l++;
-            }
+        int m = s.length();
+        int n = p.length();
 
-            // // Compare frequency Array
-            if (Arrays.equals(windowFreq, pFreq)) {
-                ans.add(i);
+        List<Integer> ans = new ArrayList<>();
+        if (m < n) {
+            return ans;
+        }
+
+        HashMap<Character, Integer> pmap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            pmap.put(p.charAt(i), pmap.getOrDefault(p.charAt(i), 0) + 1);
+        }
+
+        HashMap<Character, Integer> newmap = new HashMap<>();
+        int k = n;
+        for (int i = 0; i < k; i++) {
+            newmap.put(s.charAt(i), newmap.getOrDefault(s.charAt(i), 0) + 1);
+            if (newmap.equals(pmap)) {
+                ans.add(i - k + 1);
             }
-            i++;
+        }
+        for (int i = k; i < m; i++) {
+            newmap.put(s.charAt(i), newmap.getOrDefault(s.charAt(i), 0) + 1);
+
+            char remochar = s.charAt(i - k);
+            newmap.put(remochar, newmap.get(remochar) - 1);// if new char allready present in it just reduce its freq
+            if (newmap.get(remochar) == 0) {
+                newmap.remove(remochar);// Remove the key if its frequency reaches zero
+            }
+            
+            // compare both maps
+            if (newmap.equals(pmap)) {
+                ans.add(i - k + 1);
+            }
         }
         return ans;
     }
