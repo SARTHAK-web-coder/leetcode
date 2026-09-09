76e8ab9bddb6148ba1b248bc44764843a6d7cdac
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-09 14:47:02 +0530

    Time: 8 ms (95.34%) | Memory: 46.7 MB (82.81%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,38 +1,28 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        int m = s.length();
-        int n = p.length();
-
+        int n = s.length();
+        int k = p.length();
         List<Integer> ans = new ArrayList<>();
-        if (m < n) {
+        if(k>n){
             return ans;
         }
-
-        HashMap<Character, Integer> pmap = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            pmap.put(p.charAt(i), pmap.getOrDefault(p.charAt(i), 0) + 1);
+        int a[] = new int[26];
+        int b[] = new int[26];
+        for(int i=0;i<k;i++){
+            a[p.charAt(i)-'a']++;
         }
-
-        HashMap<Character, Integer> newmap = new HashMap<>();
-        int k = n;
-        for (int i = 0; i < k; i++) {
-            newmap.put(s.charAt(i), newmap.getOrDefault(s.charAt(i), 0) + 1);
-            if (newmap.equals(pmap)) {
-                ans.add(i - k + 1);
-            }
+        for(int i=0;i<k;i++){
+            b[s.charAt(i)-'a']++;
         }
-        for (int i = k; i < m; i++) {
-            newmap.put(s.charAt(i), newmap.getOrDefault(s.charAt(i), 0) + 1);
+        if(Arrays.equals(a,b)){
+            ans.add(0);
+        }
+        for(int i=k;i<n;i++){
+            b[s.charAt(i)-'a']++;
+            b[s.charAt(i-k)-'a']--;
 
-            char remochar = s.charAt(i - k);
-            newmap.put(remochar, newmap.get(remochar) - 1);// if new char allready present in it just reduce its freq
-            if (newmap.get(remochar) == 0) {
-                newmap.remove(remochar);// Remove the key if its frequency reaches zero
-            }
-            
-            // compare both maps
-            if (newmap.equals(pmap)) {
-                ans.add(i - k + 1);
+            if(Arrays.equals(a,b)){
+                ans.add(i-k+1);
             }
         }
         return ans;
