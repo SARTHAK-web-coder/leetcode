cd342aebb6ab28efae81b1128d8fcd4fbca39fc7
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-10 15:18:29 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (50.78%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,11 +1,13 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i =2;
-        int k=2;
+        int i = 2;
+        int k = 2;
         int n = nums.length;
-        while(i<n){
-            if(nums[i]!=nums[k-2]){
-                nums[k]=nums[i];
+        while (i < n) {
+            // if <=2 nums[element]present not affect array 
+            // but >2 nums[element] present then it (nums[k])change to next the nums[element]
+            if (nums[i] != nums[k - 2]) {
+                nums[k] = nums[i];
                 k++;
             }
             i++;
