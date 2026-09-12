be1f616239b2d62c37ed32d5d3983165cd6882b1
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-13 01:54:27 +0530

    Time: 9 ms (59.54%) | Memory: 46.2 MB (64.54%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,23 +1,24 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        HashMap<Character, Integer> map = new HashMap<>();
+        // HashMap<Character, Integer> map = new HashMap<>();
+        int a[] = new int[26];
         int maxVal = 0;
         int max = 0;
         int l = 0;
         int r = 0;
         while (r < n) {
             char ch = s.charAt(r);
-            int count = map.getOrDefault(ch, 0) + 1;
-            map.put(ch, count);
-            if (count > max) {
-                max = count;
-            }
-            // max = Math.max(max ,count);
+            a[ch - 'A']++;
+            // int count = map.getOrDefault(ch, 0) + 1;
+            // map.put(ch, count);
+            max = Math.max(max ,a[ch-'A']);
             int window = r - l + 1;
             if (window - max > k) {
-                int count2 = map.getOrDefault(s.charAt(l), count) - 1;
-                map.put(s.charAt(l), count2);
+                // int count2 = map.getOrDefault(s.charAt(l), count) - 1;
+                char chr = s.charAt(l);
+                a[chr-'A']--;
+                // map.put(s.charAt(l), count2);
                 l++;
             }
             maxVal = Math.max(maxVal, r - l + 1);
