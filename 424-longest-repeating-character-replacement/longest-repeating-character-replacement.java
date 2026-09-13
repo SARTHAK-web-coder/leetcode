f053218b7b26e4cbd4fb0adb0cea6db480c1f59d
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-13 15:26:03 +0530

    Time: 6 ms (98.97%) | Memory: 46.2 MB (50.54%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,29 +1,27 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         int n = s.length();
-        // HashMap<Character, Integer> map = new HashMap<>();
-        int a[] = new int[26];
-        int maxVal = 0;
-        int max = 0;
-        int l = 0;
-        int r = 0;
-        while (r < n) {
-            char ch = s.charAt(r);
-            a[ch - 'A']++;
-            // int count = map.getOrDefault(ch, 0) + 1;
-            // map.put(ch, count);
-            max = Math.max(max ,a[ch-'A']);
-            // int window = r - l + 1;
-            while((r - l + 1) - max > k) {
-                // int count2 = map.getOrDefault(s.charAt(l), count) - 1;
-                char chr = s.charAt(l);
-                a[chr-'A']--;
-                // map.put(s.charAt(l), count2);
-                l++;
+        int Freq[] = new int[26];
+        
+        int maxFreq = 0, maxLen = 0;
+        int left = 0, right = 0;
+
+        while (right < n) {
+            char ch = s.charAt(right);
+            Freq[ch - 'A']++;
+
+            maxFreq = Math.max(maxFreq, Freq[ch - 'A']);
+
+            while ((right - left + 1) - maxFreq > k) { // window size = (right - left + 1);
+                char lch = s.charAt(left);
+                Freq[lch - 'A']--;
+                left++;
             }
-            maxVal = Math.max(maxVal, r - l + 1);
-            r++;
+
+            maxLen = Math.max(maxLen, right - left + 1);
+
+            right++;
         }
-        return maxVal;
+        return maxLen;
     }
 }
