b50122c276f527ccc30972e2af3c424b6502fc3d
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-19 19:49:15 +0530

    Time: 6 ms (31.91%) | Memory: 44.8 MB (84.66%) - LeetSync

--- 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.java
@@ -2,20 +2,28 @@ class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int n = nums1.length, m = nums2.length;
-        int i = 0, j = 0;
-        int index = 0;
-        while (i < n && j < m) {
-            if (nums1[i] == nums2[j]) {
-                nums1[index++] = nums1[i];
-                i++;
-                j++;
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+        List <Integer> set = new ArrayList<>();
+        int n = nums1.length;
+        int m = nums2.length;
+        int l=0;
+        int r=0;
+        while(l<n && r<m){
+            if(nums1[l]>nums2[r]){
+                r++;
+            }else if(nums1[l]<nums2[r]){
+                l++;
+            }
+            else{
+                set.add(nums1[l]);
+                l++;
+                r++;
             }
         }
-        return Arrays.copyOfRange(nums1, 0, index);
+        int arr[]=new int[set.size()];
+        int i=0;
+        for(int val : set){
+            arr[i++]=val;
+        }
+        return arr;
     }
 }
