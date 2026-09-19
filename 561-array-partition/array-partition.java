231f99d77714bc8d6664ed52a27cc54e3177b249
Jagtap Sarthak <222630277+SARTHAK-web-coder@users.noreply.github.com>
2026-09-19 22:41:13 +0530

    Time: 20 ms (10.50%) | Memory: 49.8 MB (34.82%) - LeetSync

--- 561-array-partition/array-partition.java
+++ 561-array-partition/array-partition.java
@@ -1,17 +1,7 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
         int n = nums.length;
-        for (int i = 0; i < n - 1; i++) {
-            int k = i;
-            for (int j = i; j < n; j++) {
-                if (nums[j] < nums[k]) {
-                    k = j;
-                }
-            }
-            int temp = nums[i];
-            nums[i] = nums[k];
-            nums[k] = temp;
-        }
+        Arrays.sort(nums);
         int i = 0;
         int sum = 0;
         while (i < n - 1) {
